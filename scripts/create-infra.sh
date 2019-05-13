ce566bd606137d994064ee0178fe9f277e7df419
creanga <creanga@adobe.com>
2019-05-13 17:28:41 +0300

    tiny change on script create

--- scripts/create-infra.sh
+++ scripts/create-infra.sh
@@ -5,7 +5,6 @@ set -x #echo on
 kubectl delete ns production  --grace-period=0 && sleep 180 || true
 kubectl create ns production
 kubectl --namespace=production apply -f k8/production
-kubectl --namespace=production apply -f k8/canary
 kubectl --namespace=production apply -f k8/services
 
-kubectl --namespace=production get service prime-palindrome
+kubectl --namespace=production get service realtime-dispatcher
